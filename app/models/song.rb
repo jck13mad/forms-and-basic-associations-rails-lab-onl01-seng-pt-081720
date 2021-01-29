a3643e8ffb8fe3f06dff123bcbbe00c9f36effea
Jack Madden <jck13mad@gmail.com>
2021-01-29 00:30:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   
   def note_contents=(content)
     content.each do |content|
-      self.notes << Note
+      self.notes << Note.find_or_create_by(content: content)
     end 
   end 
 end
